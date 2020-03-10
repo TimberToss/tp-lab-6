db90d0e40394bfa7f16cc718b7ea4083e382fd41
TimberToss <tarshinmm@mail.ru>
2020-03-11 00:35:21 +0300

    Third

--- include/Project.h
+++ include/Project.h
@@ -1,5 +1,28 @@
 #pragma once
+#include <vector>
+#include "Employee.h"
+#include "Manager.h"
+#include "Engineer.h"
 
 class Project {
+public:
+    Project(std::string name, std::vector<Employee*> staff, Manager *manager);
 
+    ~Project();
+
+    std::string getName();
+    void setName(std::string name);
+
+    std::vector<Employee*> getStaff();
+    void setStaff(std::vector<Employee*> staff);
+
+    Manager *getManager();
+    void setManager(Manager *manager);
+
+    void addEngineer(Engineer* engineer);
+
+private:
+    std::string name;
+    std::vector<Employee*> staff;
+    class Manager* manager;
 };
--- src/Project.cpp
+++ src/Project.cpp
@@ -1,2 +1,40 @@
 
 #include "Project.h"
+
+Project::~Project() {
+
+}
+
+std::string Project::getName() {
+    return name;
+}
+
+void Project::setName(std::string name) {
+    this->name = name;
+}
+
+std::vector<Employee*> Project::getStaff() {
+    return staff;
+}
+
+void Project::setStaff(std::vector<Employee *> staff) {
+    this->staff = staff;
+}
+
+Manager *Project::getManager() {
+    return manager;
+}
+
+void Project::setManager(Manager *manager) {
+    this->manager = manager;
+}
+
+Project::Project(std::string name, std::vector<Employee*> staff, Manager *manager) {
+    this->name = name;
+    this->staff = staff;
+    this->manager = manager;
+}
+
+void Project::addEngineer(Engineer *engineer) {
+    staff.push_back(engineer);
+}
