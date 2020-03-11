944ed1332c5aec0ccc8e3da28e6c0cc52f92d541
TimberToss <51414566+TimberToss@users.noreply.github.com>
2020-03-11 09:00:21 +0300

    Update Tester.cpp

--- src/Tester.cpp
+++ src/Tester.cpp
@@ -1,6 +1,7 @@
 #include "../include/Tester.h"
 
-Tester::Tester(std::string name, int id, int workTime, int payment) : Engineer(name, id, workTime, payment, 0) {}
+Tester::Tester(std::string name, int id, int workTime, int payment, int partOfProject, int projectImportance)
+    : Engineer(name, id, workTime, payment, partOfProject, projectImportance) {}
 
 Tester::~Tester() {}
 
